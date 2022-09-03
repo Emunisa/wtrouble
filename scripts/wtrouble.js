579d533847535b050ec111f2ad9840259107c116
Adam Kalman <dkeka.zkfaks+github@gmail.com>
2022-09-02 20:23:08 -0700

    create generic functions for rolling and have all types use those

--- scripts/wtrouble.js
+++ scripts/wtrouble.js
@@ -6,173 +6,134 @@ function rollAttributes() {
 	materia();
 }
 
-function characters() {
-	const characters = [
-	'Aerith', 'Tifa', 'Barret', 'Yuffie', 'Vincent', 'Cid', 'Cait Sith'
-	];
+const characters = [ 'Aerith', 'Tifa', 'Barret', 'Yuffie', 'Vincent', 'Cid', 'Cait Sith' ];
+const equipment = [ 'Weapon', 'Armor', 'Accessory' ];
+const limits = [ '1-1', '1-2', '2-1', '2-2', '3-1', '3-2', '4-1' ]
+const materia = [ 'Magic', 'Command', 'Summon', 'Support', 'Independent' ];
+const events = [
+	'Breed Gold Chocobo',
+	'Beat Emerald',
+	'Beat Ruby',
+	'Complete Ancient Forest',
+	'Beat Ultimate',
+	'Defeat Raps (Wutai)',
+	'Finish Pagoda (Wutai)',
+	'Finish Mogs House',
+	'Achieve S Rank Chocobo',
+	'Collect all Summon Materia',
+	'Beat Gelnika Turks'
+];
 
-	var char2 = Math.floor(Math.random() * characters.length);
-	var char3 = Math.floor(Math.random() * characters.length);
-	var char3b = Math.floor(Math.random() * characters.length);
+const valueToIconMap = {
+	'Weapon': 'FFVII_Sword_Icon.PNG',
+	'Armor': 'FFVII_Armor_Icon.PNG',
+	'Accessory': 'FFVII_Accessory_Icon.PNG',
+	'Magic': 'FFVII_Magic_Materia_Icon.PNG',
+	'Command': 'FFVII_Command_Materia_Icon.PNG',
+	'Summon': 'FFVII_Summon_Materia_Icon.PNG',
+	'Support': 'FFVII_Support_Materia_Icon.PNG',
+	'Independent': 'FFVII_Independent_Materia_Icon.PNG'
+};
 
-	while (char2 == char3) {
-		char3 = Math.floor(Math.random() * characters.length);
+// note: don't do this
+Array.prototype.shuffle = function shuffle() {
+	let copy = this.concat();
+	let currentIdx = copy.length;
+
+	while (currentIdx != 0) {
+		let targetIdx = Math.floor(Math.random() * currentIdx);
+		currentIdx--;
+
+		let temp = copy[currentIdx];
+		copy[currentIdx] = copy[targetIdx];
+		copy[targetIdx] = temp;
 	}
 
-	if (characters[char2] == "Aerith" || characters[char3] == "Aerith") {	
-		while (characters[char3b] == "Aerith" || char3b == char2 || char3b == char3) {
-			char3b = Math.floor(Math.random() * characters.length);
+	return copy;
+}
+
+const getInnerHTML = (values, take) => {
+	let innerHtml = '';
+
+	while (take != 0) {
+		// pull first value of randomly ordered array
+		let value = values.pop();
+
+		// check to see if there is an icon and add that
+		if (value in valueToIconMap) {
+			innerHtml += `<img src=assets/${valueToIconMap[value]} />`;
 		}
-	} 
+
+		// add the value`
+		innerHtml += value;
+
+		// if its aerith add the backup value
+		if (value == 'Aerith') {
+			innerHtml += ` (${values.pop()})`;
+		}
+
+		take--;
+
+		if (take != 0) {
+			innerHtml += ', ';
+		}
+	}
+
+	return innerHtml;
+}
+
+function setCharacters() {
+	let chars = characters.shuffle();
 
 	document.getElementById("charRes1").innerHTML = "Cloud";
-	
-	if (characters[char2] == "Aerith") {
-	document.getElementById("charRes2").innerHTML = characters[char2] + " (" + characters[char3b] + ")";
-	document.getElementById("charRes3").innerHTML = characters[char3];
-	}
-	else if(characters[char3] == "Aerith") {
-	document.getElementById("charRes3").innerHTML = characters[char3] + " (" + characters[char3b] + ")";
-	document.getElementById("charRes2").innerHTML = characters[char2];
-	}
-	else {
-	document.getElementById("charRes2").innerHTML = characters[char2];
-	document.getElementById("charRes3").innerHTML = characters[char3];
-	}
+	document.getElementById("charRes2").innerHTML = getInnerHTML(chars, 1);
+	document.getElementById("charRes3").innerHTML = getInnerHTML(chars, 1);
 }
 
-function equipment() {
-	const equipment = [
-	'Weapon', 'Armor', 'Accessory'
-	];
-	
-	const eqimage = [
-	'FFVII_Sword_Icon.PNG', 'FFVII_Armor_Icon.PNG', 'FFVII_Accessory_Icon.PNG'
-	];
-	
-	if (document.getElementById("indpEq").checked){
-	} else {
-		var eq1 = Math.floor(Math.random() * equipment.length);
-		var eq2 = Math.floor(Math.random() * equipment.length);
-		while (eq1 == eq2) {
-			eq2 = Math.floor(Math.random() * equipment.length);
-		}
-		document.getElementById("eqRes1").innerHTML = "<img src=assets/" + eqimage[eq1] + "> " + equipment[eq1] + ", <img src=assets/" + eqimage[eq2] + "> "  + equipment[eq2];
-		document.getElementById("eqRes2").innerHTML = "<img src=assets/" + eqimage[eq1] + "> " + equipment[eq1] + ", <img src=assets/" + eqimage[eq2] + "> "  + equipment[eq2];
-		document.getElementById("eqRes3").innerHTML = "<img src=assets/" + eqimage[eq1] + "> " + equipment[eq1] + ", <img src=assets/" + eqimage[eq2] + "> "  + equipment[eq2];
+function setAllEquipment() {
+	if (document.getElementById("indpEq").checked) {
+		return;
 	}
 
+	let equips = equipment.shuffle();
+	let innerHtml = getInnerHTML(equips, 2);
+
+	document.getElementById("eqRes1").innerHTML = innerHtml;
+	document.getElementById("eqRes2").innerHTML = innerHtml;
+	document.getElementById("eqRes3").innerHTML = innerHtml;
 }
 
-function rolleq1() {
-	if (document.getElementById("indpEq").checked){
-		const equipment = [
-		'Weapon', 'Armor', 'Accessory'
-		];
-		
-		const eqimage = [
-		'FFVII_Sword_Icon.PNG', 'FFVII_Armor_Icon.PNG', 'FFVII_Accessory_Icon.PNG'
-		];	
-		
-		var eq1 = Math.floor(Math.random() * equipment.length);
-		var eq2 = Math.floor(Math.random() * equipment.length);
-		
-		while (eq1 == eq2) {
-		eq2 = Math.floor(Math.random() * equipment.length);
-		}
-		
-		document.getElementById("eqRes1").innerHTML = "<img src=assets/" + eqimage[eq1] + "> " + equipment[eq1] + ", <img src=assets/" + eqimage[eq2] + "> "  + equipment[eq2];
+function setEquipment(num) {
+	if (!document.getElementById("indpEq").checked) {
+		return;
 	}
+
+	let equips = equipment.shuffle();
+	let innerHtml = getInnerHTML(equips, 2);
+
+	document.getElementById(`eqRes${num}`).innerHTML = innerHtml;
 }
 
-function rolleq2() {
-	if (document.getElementById("indpEq").checked){
-		const equipment = [
-		'Weapon', 'Armor', 'Accessory'
-		];
-		
-		const eqimage = [
-		'FFVII_Sword_Icon.PNG', 'FFVII_Armor_Icon.PNG', 'FFVII_Accessory_Icon.PNG'
-		];	
-		
-		var eq1 = Math.floor(Math.random() * equipment.length);
-		var eq2 = Math.floor(Math.random() * equipment.length);
-		
-		while (eq1 == eq2) {
-		eq2 = Math.floor(Math.random() * equipment.length);
-		}
-		
-		document.getElementById("eqRes2").innerHTML = "<img src=assets/" + eqimage[eq1] + "> " + equipment[eq1] + ", <img src=assets/" + eqimage[eq2] + "> "  + equipment[eq2];
-	}
+function setEvents() {
+	let evnts = events.shuffle();
+	document.getElementById("EventResult").innerHTML = getInnerHTML(evnts, 2);
 }
 
-function rolleq3() {
-	if (document.getElementById("indpEq").checked){
-		const equipment = [
-		'Weapon', 'Armor', 'Accessory'
-		];
-		
-		const eqimage = [
-		'FFVII_Sword_Icon.PNG', 'FFVII_Armor_Icon.PNG', 'FFVII_Accessory_Icon.PNG'
-		];	
-		
-		var eq1 = Math.floor(Math.random() * equipment.length);
-		var eq2 = Math.floor(Math.random() * equipment.length);
-		
-		while (eq1 == eq2) {
-		eq2 = Math.floor(Math.random() * equipment.length);
-		}
-		
-		document.getElementById("eqRes3").innerHTML = "<img src=assets/" + eqimage[eq1] + "> " + equipment[eq1] + ", <img src=assets/" + eqimage[eq2] + "> "  + equipment[eq2];
-	}
+function setLimits() {
+	let lmts = limits.shuffle();
+	document.getElementById("LimitResult").innerHTML = getInnerHTML(lmts, 1);
 }
 
-function events() {
-	const event = [
-	'Breed Gold Chocobo', 'Beat Emerald', 'Beat Ruby', 'Complete Ancient Forest', 'Beat Ultimate', 'Defeat Raps (Wutai)', 'Finish Pagoda (Wutai)', 'Finish Mogs House', 'Achieve S Rank Chocobo', 'Collect all Summon Materia', 'Beat Gelnika Turks'
-	];
-	
-	var ev1 = Math.floor(Math.random() * event.length);
-	var ev2 = Math.floor(Math.random() * event.length);
-	
-	while (ev1 == ev2) {
-		ev2 = Math.floor(Math.random() * event.length);
-	}
-	
-	document.getElementById("EventResult").innerHTML = event[ev1] + ", " + event[ev2];
+function setMateria() {
+	let mats = materia.shuffle();
+	let take = document.getElementById("noitem").checked ? 3 : 2;
+	document.getElementById("MateriaResult").innerHTML = getInnerHTML(mats, take);
 }
 
-function limit() {
-	const limit = [
-	'1-1', '1-2', '2-1', '2-2', '3-1', '3-2', '4-1'
-	];
-
-	document.getElementById("LimitResult").innerHTML = limit[Math.floor(Math.random() * limit.length)];
-}
-
-function materia() {
-	const materia = [
-	'Magic', 'Command', 'Summon', 'Support', 'Independent'
-	];
-	const materiaimage = [
-	'FFVII_Magic_Materia_Icon.PNG', 'FFVII_Command_Materia_Icon.PNG', 'FFVII_Summon_Materia_Icon.PNG', 'FFVII_Support_Materia_Icon.PNG', 'FFVII_Independent_Materia_Icon.PNG' 
-	]
-	
-	var mat1 = Math.floor(Math.random() * materia.length);
-	var mat2 = Math.floor(Math.random() * materia.length);
-	
-	while (mat1 == mat2) {
-		mat2 = Math.floor(Math.random() * materia.length);
-	}
-	if (document.getElementById("noitem").checked){
-		var mat3 = Math.floor(Math.random() * materia.length);
-		
-		while (mat3 == mat1 || mat3 == mat2) {
-		mat3 = Math.floor(Math.random() * materia.length);
-		}
-		
-		document.getElementById("MateriaResult").innerHTML = "<img src=assets/" + materiaimage[mat1] + "> " + materia[mat1] + ", <img src=assets/" + materiaimage[mat2] + "> " + materia[mat2] + ", <img src=assets/" + materiaimage[mat3] + "> " + materia[mat3];
-	} else {
-		document.getElementById("MateriaResult").innerHTML = "<img src=assets/" + materiaimage[mat1] + "> " + materia[mat1] + ", <img src=assets/" + materiaimage[mat2] + "> " + materia[mat2];
-	}
+function setAll() {
+	setCharacters();
+	setAllEquipment();
+	setEvents();
+	setLimits();
+	setMateria();
 }
